d83fe554c4cece60378b8633b844a9b51d07c66d
Grant-Bever <89789562+Grant-Bever@users.noreply.github.com>
2024-11-02 02:26:26 -0400

    Searching part of writeup
    -first half of the writeup
    -cleaned up a couple functions

--- Assignment 2/js/linearSearch.js
+++ Assignment 2/js/linearSearch.js
@@ -3,19 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.linearSearch = void 0;
 function linearSearch(arr, searchItems) {
     let totalComparisons = 0;
-    let results = [];
     for (const item of searchItems) {
-        let found = false;
         let comparisons = 0;
         for (let i = 0; i < arr.length; i++) {
             comparisons++;
             if (arr[i] === item) {
-                found = true;
                 break;
             }
         }
         totalComparisons += comparisons;
-        results.push(found);
         //console.log(`Item: ${item}, Comparisons: ${comparisons}`); //test b
     }
     const avgComparisons = parseFloat((totalComparisons / searchItems.length).toFixed(2));
--- Assignment 2/src/linearSearch.ts
+++ Assignment 2/src/linearSearch.ts
@@ -1,20 +1,16 @@
 export function linearSearch(arr: any[], searchItems: any[]): void{
     let totalComparisons = 0;
 
-    let results: boolean[] = [];
     for (const item of searchItems) {
-        let found = false;
         let comparisons = 0;
 
         for (let i = 0; i < arr.length; i++) {
             comparisons++;  
             if (arr[i] === item) {
-                found = true;
                 break;
             }
         }
         totalComparisons += comparisons;
-        results.push(found);
         //console.log(`Item: ${item}, Comparisons: ${comparisons}`); //test b
     }
     const avgComparisons = parseFloat((totalComparisons / searchItems.length).toFixed(2));
